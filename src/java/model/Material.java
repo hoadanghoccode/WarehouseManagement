dd70b1b6f84f8c50b7b0d83910984d0aa5c8540b
LeGiangne03 <gianglvhe171333@fpt.edu.vn>
2025-06-02 00:51:53 +0700

    Update model Material, Add model Unit, Supplier from new database

--- src/java/model/Material.java
+++ src/java/model/Material.java
@@ -14,16 +14,29 @@ public class Material {
     private int categoryId;
     private String name;
     private String status;
+    private String categoryName;
+    private String parentCategoryName;
+    private int unitId;
+    private String unitName;
+    private double price;
+    private double quantity;
     private int supplierId;
     private String supplierName;
 
     public Material() {}
 
-    public Material(int materialId, int categoryId, String name, String status, int supplierId, String supplierName) {
+    public Material(int materialId, int categoryId, String name, String status, String categoryName, String parentCategoryName, 
+                    int unitId, String unitName, double price, double quantity, int supplierId, String supplierName) {
         this.materialId = materialId;
         this.categoryId = categoryId;
         this.name = name;
         this.status = status;
+        this.categoryName = categoryName;
+        this.parentCategoryName = parentCategoryName;
+        this.unitId = unitId;
+        this.unitName = unitName;
+        this.price = price;
+        this.quantity = quantity;
         this.supplierId = supplierId;
         this.supplierName = supplierName;
     }
@@ -36,6 +49,18 @@ public class Material {
     public void setName(String name) { this.name = name; }
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
+    public String getCategoryName() { return categoryName; }
+    public void setCategoryName(String categoryName) { this.categoryName = categoryName; }
+    public String getParentCategoryName() { return parentCategoryName; }
+    public void setParentCategoryName(String parentCategoryName) { this.parentCategoryName = parentCategoryName; }
+    public int getUnitId() { return unitId; }
+    public void setUnitId(int unitId) { this.unitId = unitId; }
+    public String getUnitName() { return unitName; }
+    public void setUnitName(String unitName) { this.unitName = unitName; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+    public double getQuantity() { return quantity; }
+    public void setQuantity(double quantity) { this.quantity = quantity; }
     public int getSupplierId() { return supplierId; }
     public void setSupplierId(int supplierId) { this.supplierId = supplierId; }
     public String getSupplierName() { return supplierName; }
--- src/java/model/Unit.java
+++ src/java/model/Unit.java
@@ -12,23 +12,20 @@ package model;
 public class Unit {
     private int unitId;
     private String name;
+    private String status;
 
-    public Unit() {
-    }
+    public Unit() {}
 
-    public int getUnitId() {
-        return unitId;
-    }
-
-    public void setUnitId(int unitId) {
+    public Unit(int unitId, String name, String status) {
         this.unitId = unitId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
+        this.status = status;
     }
+
+    public int getUnitId() { return unitId; }
+    public void setUnitId(int unitId) { this.unitId = unitId; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
